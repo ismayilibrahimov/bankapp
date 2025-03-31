c08987b1cae060420991a872a3a45bdc0e1399ab
Ismayil Ibrahimov <e.ismayilibrahimov@gmail.com>
2025-03-31 14:52:04 +0400

    add global exception handler

---
+++ src/main/java/bankapp/config/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package bankapp.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleException(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
